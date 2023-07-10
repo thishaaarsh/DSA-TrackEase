6ff5150a57b19dcf5e6916a4117a6e484cf05c84
thishaaarsh <harshjainh.j.35@gmail.com>
2023-07-10 23:52:17 +0530

    pushing project

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Badge from "react-bootstrap/Badge";
+import { Link } from "react-router-dom";
+import "./footer.css";
+export default function Footer({ dark, setDark }) {
+	return (
+		<div>
+			<footer className="footer" style={{ backgroundColor: dark ? "#393E46" : "" }}>
+				<div className="d-flex bd-highlight">
+					<div className="p-2 bd-highlight">
+						
+					</div>
+					<div className="ml-auto p-2 bd-highlight footer-toggle">
+						<h4>
+							{/* toggle dark mode */}
+							<Badge
+								pill
+								variant="light"
+								className="hvr-grow"
+								onClick={() => {
+									setDark(!dark);
+									window.localStorage["isDark"] = !dark;
+								}}
+								style={{ cursor: "pointer" }}
+							>
+								{dark ? (
+									<span role="img" aria-label="sun-emoji" className="emojiFix">
+										☀️
+									</span>
+								) : (
+									<span role="img" aria-label="moon-emoji" className="emojiFix">
+										🌙
+									</span>
+								)}
+							</Badge>{" "}
+							
+						</h4>
+					</div>
+				</div>
+			</footer>
+		</div>
+	);
+}
